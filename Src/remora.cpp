166aa7ee8b2b15a9ef3c6eb9b7273dd797eaf1a9
andrew_marles <andrew@expatria.ca>
2023-09-20 17:51:48 -0700

    Removed unused file, minor notations.

--- Src/remora.cpp
+++ Src/remora.cpp
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "configuration.h"
 #include "remora.h"
-#include "boardconfig.h"
+//#include "boardconfig.h"
 
 #include "crc32.h"
 
